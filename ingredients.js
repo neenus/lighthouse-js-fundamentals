2ff6f42c598a7d3644315a7fd22b570d34be6e11
Neenus Gabriel <neenus@live.ca>
2018-07-09 04:18:34 +0000

    looping & repetition assignment

---
+++ ingredients.js
@@ -0,0 +1,18 @@
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+// Write a while loop that prints out the contents of ingredients:
+let i = 0;
+while (i < ingredients.length) {
+  console.log(ingredients[i]);
+  i +=1;
+}
+// Write a for loop that prints out the contents of ingredients:
+for (var i = 0; i < ingredients.length; i += 1) {
+  console.log(ingredients[i]);
+}
+
+// Write any loop (while or for) that prints out the contents of ingredients backwards:
+let revIngredients = ingredients.reverse();
+for (var i = 0; i < revIngredients.length; i += 1) {
+  console.log(revIngredients[i]);
+}
